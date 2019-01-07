46322fc2d3081cb0d81fb251a32a94a2c99aa4c6
Edison Neto <ednetoali@gmail.com>
2019-01-06 23:46:39 -0200

    Add setter methods to user class

--- src/main/java/org/spring/User.java
+++ src/main/java/org/spring/User.java
@@ -1,20 +1,35 @@
 package org.spring;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class User {
 
-    private int id;
+    @NotNull
+    private Integer id;
+
+    @NotNull
+    @Size(min=1, max=20)
     private String name;
 
-    public User(int id, String name) {
+    public User(Integer id, String name) {
         this.id = id;
         this.name = name;
     }
 
-    public long getId() {
+    public Integer getId() {
         return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
