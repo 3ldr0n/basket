5e5aca02d57add030bacce0d436c39eda8cdc346
Edison Neto <ednetoali@gmail.com>
2019-01-22 15:56:34 -0200

    Update dependencies

--- src/main/java/org/spring/App.java
+++ src/main/java/org/spring/App.java
@@ -2,9 +2,17 @@ package org.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class App {
+
+    @RequestMapping("/")
+    public String Hello() {
+        return "Hello";
+    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(App.class, args);
