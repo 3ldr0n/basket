19b47583e0cc12916962fd0c382456ce7f4eb102
Edison Neto <ednetoali@gmail.com>
2019-02-23 16:21:49 -0300

    Update repository for new project

--- src/main/java/org/basket/BasketApplication.java
+++ src/main/java/org/basket/BasketApplication.java
@@ -1,14 +1,13 @@
-package org.blog;
+package org.basket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BlogApplication {
+public class BasketApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BlogApplication.class, args);
+		SpringApplication.run(BasketApplication.class, args);
 	}
 
 }
-
--- src/test/java/org/basket/BasketApplicationTests.java
+++ src/test/java/org/basket/BasketApplicationTests.java
@@ -1,4 +1,4 @@
-package org.blog.blog;
+package org.basket;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,11 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BlogApplicationTests {
+public class BasketApplicationTests {
 
 	@Test
 	public void contextLoads() {
 	}
 
 }
-
